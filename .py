7a0274d8072fb70e975020cf6ff697d49febf1a3
ETSHA1 <elijahshanawaz@gmail.com>
2025-09-11 12:34:26 +1000

    Update and rename .gitignore to .py
    work

---
+++ .py
@@ -0,0 +1,10 @@
+
+
+'Create main.py with a variable called stop that is set to False.'
+'Then, create a while stop == False loop. Inside the loop, the menu should  do the following:'
+'Print "1: About/Team" 
+'If the user types 1: the menu prints the about us message.' 
+'Print "2: Our Next Feature" 
+'If the user types 2: the menu prints "Coming Soon!" 
+'Print "3: Close Program" 
+'If the user types 3: The program sets stop to True'
