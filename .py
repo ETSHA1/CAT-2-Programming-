640acc412c5b010417bfc8892b2e5e858060ddfe
ETSHA1 <elijahshanawaz@gmail.com>
2025-09-11 17:51:43 +1000

    Update .py i should have finished it

--- .py
+++ .py
@@ -1,15 +1,20 @@
 #looping not done
-def i():
-    i is false
-while i is False:
+stop = False  #loop
+
+while stop == False:
+    # print menu 
+    print("Menu please chode a number:")
+    print("1: About/Team")
+    print("2: Our Next Feature")
+    print("3: Close Program")
     menu_number = int(input("Enter 1 for About/Team Enter 2 for our next feature Enter 3 for closing our program."))
     if menu_number == 1:
         print("insert about us message")
     elif menu_number == 2:
         print("Coming Soon!")
     elif menu_number == 3:
-        i is True
         print("Exiting code")
+        stop is True #ts is for exit 
     else:
         print("Invalid input!")
   
