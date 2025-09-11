e7be024be2c13df15aba2b6d8dcdaa2affaf7b6d
PacificOC1 <dabking6661@gmail.com>
2025-09-12 08:50:51 +1000

    Update 1.1 .py

--- .py
+++ .py
@@ -1,30 +1,22 @@
 #looping not done
 stop = False  #loop
 
-while stop == False:
+while not stop:
     # print menu 
-    print("Menu please chode a number:")
-    print("1: About/Team")
-    print("2: Our Next Feature")
-    print("3: Close Program")
-    menu_number = int(input("please select a number."))
+    print("Menu: Please choose a number:\n1: About/Team\n2: Our Next Feature:\n3: Close Program")
+    menu_number = int(input("Please select a number."))
     if menu_number == 1:
         print("insert about us message")
     elif menu_number == 2:
         print("Coming Soon!")
     elif menu_number == 3:
         print("Exiting code")
-        stop is True #ts is for exit 
+        stop = True #ts is for exit 
+        
     else:
         print("Invalid input!")
   
   
 
-'Create main.py with a variable called stop that is set to False.'
-'Then, create a while stop == False loop. Inside the loop, the menu should  do the following:'
-'Print "1: About/Team" 
-'If the user types 1: the menu prints the about us message.' 
-'Print "2: Our Next Feature" 
-'If the user types 2: the menu prints "Coming Soon!" 
-'Print "3: Close Program" 
-'If the user types 3: The program sets stop to True'
+
+
