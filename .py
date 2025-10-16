e4899d53ae07b95052a92e19d875bb61c1e86933
ETSHA1 <elijahshanawaz@gmail.com>
2025-10-17 08:39:37 +1100

    Update .py

--- .py
+++ .py
@@ -1,52 +1,41 @@
 import random #gives us random thingys
 
-class Card:
-    def __init__(self, suit, number):
-        self.suit = suit
-        self.number = number
+class Card:#:#make card 
+    def __init__(self, suit, number):#store an empty list of cards
+        self.suit = suit #the empty list of suit
+        self.number = number# the numbers
 
-    def __repr__(self):
+    def __repr__(self): #creates a string using placeholder values
         return f"{self.number} of {self.suit}"
 
-    @property
+    @property #the value of self._suit stored within the my_card
     def suit(self):
         return self._suit
 
-    @suit.setter
+    @suit.setter# checks if data is a real suit
     def suit(self, suit):
         if suit in ["hearts", "clubs", "diamonds", "spades"]:
             self._suit = suit
         else:
             raise ValueError("That's not a valid suit!")
 
-class Deck:
+class Deck:# makes deck for us to use
     def __init__(self):
         self._cards = []
         self.populate()
 
-    def populate(self):
+    def populate(self):#makes deck have cards
         suits = ["hearts", "clubs", "diamonds", "spades"]
         numbers = [str(n) for n in range(2, 11)] + ["J", "Q", "K", "A"]
         self._cards = [Card(suit, number) for suit in suits for number in numbers]
 
-    def shuffleDeck(self):
+    def shuffleDeck(self):#use random module to randomize card
         random.shuffle(self._cards)
 
-    def showTopCards(self, count=3):
-        return self._cards[:count]
+    def showTopCards(self, count=3):#shows top card on deck by selecting first 3 card in cards
+        return self._cards[:count] 
 
-    def dealCards(self, count=5):
-        if count > len(self._cards):
-            raise ValueError("Not enough cards left to deal!")
-        dealt = self._cards[:count]
-        self._cards = self._cards[count:]
-        return dealt
-
-    def resetDeck(self):
-        self.populate()
-
-    def count(self):
-        return len(self._cards)
+ 
 
     def highestCard(self, cards):
         order = {str(n): n for n in range(2, 11)}
@@ -62,33 +51,3 @@ def describeProject():
     print("- Resets deck and confirms count = 52")
     print("- Validates input when dealing")
 
-def test():
-    describeProject()
-    deck = Deck()
-    print("\nInitial deck count:", deck.count())
-
-    deck.shuffleDeck()
-    print("\nTop 3 cards after shuffle:")
-    for card in deck.showTopCards():
-        print(card)
-
-    print("\nDealing 5 cards...")
-    hand = deck.dealCards()
-    for card in hand:
-        print(card)
-
-    print("\nHighest card in hand:", deck.highestCard(hand))
-
-    print("\nDeck count after dealing:", deck.count())
-
-    print("\nResetting deck...")
-    deck.resetDeck()
-    print("Deck count after reset:", deck.count())
-
-    print("\nTrying to deal 60 cards (should fail):")
-    try:
-        deck.dealCards(60)
-    except ValueError as e:
-        print("Error:", e)
-
-test()
