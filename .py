422c733d96d709297657e989331621e4d5a18841
ETSHA1 <elijahshanawaz@gmail.com>
2025-10-17 09:20:51 +1100

    Update .py

--- .py
+++ .py
@@ -1,17 +1,3 @@
-#looping not done
-i is false
-while i is false:
-#elif path for the code
-menunumber = int(input(" enter 1 for about/team enter 2 for our next feature enenter 3 for closing our program"))
-if menunumber == 1:
-  print("
-  
-
-'Create main.py with a variable called stop that is set to False.'
-'Then, create a while stop == False loop. Inside the loop, the menu should  do the following:'
-'Print "1: About/Team" 
-'If the user types 1: the menu prints the about us message.' 
-'Print "2: Our Next Feature" 
-'If the user types 2: the menu prints "Coming Soon!" 
-'Print "3: Close Program" 
-'If the user types 3: The program sets stop to True'
+    stop = False
+    while not stop:
+      print('hello world')
