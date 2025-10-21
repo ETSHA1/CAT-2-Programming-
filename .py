352a9f3037027c9cffdecaad7fb04641822d9eb6
ETSHA1 <elijahshanawaz@gmail.com>
2025-10-22 08:55:19 +1100

    Update .py

--- .py
+++ .py
@@ -1,10 +1,10 @@
 stop = False  #loop
 while not stop:
     # print menu
-    print("Menu:\nWelcome to Gambit: Elijah & Charlie’s Showdown! Please choose a number:\n1: About/Team\n2: Our Next Feature:\n3: Close Program")
+    print("Menu:\nWelcome to Elijah & Charlie’s card deck game. Please choose a number:\n1: About/Team\n2: Our Next Feature:\n3: Close Program")
     menu_number = int(input("Please select a number. "))
     if menu_number == 1:
-        print("We are a small company that specialises in mini-games based in Python!")
+        print("We are a small company that specialises in games based in Python!")
     elif menu_number == 2:
         print("Coming Soon!")
     elif menu_number == 3:
